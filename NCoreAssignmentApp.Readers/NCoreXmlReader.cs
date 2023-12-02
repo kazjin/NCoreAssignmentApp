80a0450dfbaa32c4d19e96353431aa3d5a2433d4
sergey.pupinin <sergey.pupinin@hotmail.com>
2023-12-02 17:37:39 +0100

    added xmlreader

---
+++ NCoreAssignmentApp.Readers/NCoreXmlReader.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System.Xml;
+
+namespace NCoreAssignmentApp.Readers
+{
+    public class NCoreXmlReader : IReader
+    {
+        public async Task<string> ReadContent(string filePath)
+        {
+            XmlReaderSettings settings = new()
+            {
+                Async = true
+            };
+
+            StringBuilder builder = new();
+
+            using (XmlReader reader = XmlReader.Create(filePath, settings))
+            {
+                while (await reader.ReadAsync())
+                {
+                    switch (reader.NodeType)
+                    {
+                        case XmlNodeType.Element:
+                            builder.Append($"\n{reader.Name} ");
+                            break;
+                        case XmlNodeType.Text:
+                            builder.Append($"text node: {await reader.GetValueAsync()}");
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+
+            return builder.ToString();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,9 +8,43 @@ namespace NCoreAssignmentApp
         {
             Console.WriteLine("Hello, welcome to NCoreAssignmentApp!");
 
-            var filePath = RequestFilePath();
+            var chosenKey = ShowMenu();
 
-            await ReadTextAndWriteToConsole(filePath);
+            await ProcessChoice(chosenKey);
+        }
+
+        private static async Task ProcessChoice(ConsoleKeyInfo chosenKey)
+        {
+            string filePath;
+            switch (chosenKey.Key)
+            {
+                case ConsoleKey.NumPad1:
+                case ConsoleKey.D1:
+                    Console.WriteLine("\nSelected to read a text file");
+                    filePath = RequestFilePath();
+                    await ReadTextAndWriteToConsole(filePath);
+                    break;
+                case ConsoleKey.NumPad2:
+                case ConsoleKey.D2:
+                    Console.WriteLine("\nSelected to read a xml file");
+                    filePath = RequestFilePath();
+                    await ReadXmlAndWriteToConsole(filePath);
+                    break;
+                default:
+                    Console.WriteLine("\nInput is not valid. Try again");
+                    chosenKey = ShowMenu();
+                    await ProcessChoice(chosenKey);
+                    break;
+            }
+        }
+
+        private static ConsoleKeyInfo ShowMenu()
+        {
+            Console.WriteLine("Choose which file type to read:");
+            Console.WriteLine("1. text file");
+            Console.WriteLine("2. xml file");
+            var choiceKey = Console.ReadKey();
+            return choiceKey;
         }
 
         private static string RequestFilePath()
@@ -35,5 +69,13 @@ namespace NCoreAssignmentApp
             Console.WriteLine(content);
         }
 
+        private static async Task ReadXmlAndWriteToConsole(string filePath)
+        {
+            var nCoreTextReader = new NCoreXmlReader();
+            var content = await nCoreTextReader.ReadContent(filePath);
+
+            Console.WriteLine("The contents of the file:");
+            Console.WriteLine(content);
+        }
     }
 }
